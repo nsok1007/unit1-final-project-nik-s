c1aebc1d60e27bf75aff4c4f73b30422763e9fee
nsok1007 <nsok1007@gmail.com>
2026-08-17 17:04:18 -0500

    wip: add hardcoded mock data

---
+++ src/mockData.js
@@ -0,0 +1,112 @@
+const defaultCopingTools = [
+    {
+        id: "cskill1",
+        name: "5-4-3-2-1",
+        description: "Using your five senses: What are 5 things you can see? What are 4 things you can feel? What are 3 things you can hear? What are 2 things you can smell? 1 thing you can taste?",
+        category: "mindfulness",
+        therapy: ["ACT", "DBT"],
+    },
+     {
+        id: "cskill2",
+        name: "Act Opposite",
+        description: "The next time you have the urge to do something when stress is high, try doing the opposite of that and see how you feel.",
+        category: "emotion regulation",
+        therapy: "DBT",
+    },
+     {
+        id: "cskill3",
+        name: "Box Breathing",
+        description: "Take a deep breath and notice how your belly fills with air. Exhale deeply and do the following: Inhale for 4 counts. Hold your breath for 4 counts. Exhale for 4 counts. Repeat as long as what feels right.",
+        category: "mindfulness",
+        therapy: "DBT",
+    },
+     {
+        id: "cskill4",
+        name: "Leaves on a Stream",
+        description: "Imagine you're sitting by a stream. Notice the soft grass beneath you as the the sun's rays feel warm against your face. Now, imagine that your thoughts are leaves floating down the river. What feelings and sensations come up? Can you notice the leaves move without narrowing in on them?",
+        category: "mindfulness",
+        therapy: "ACT",
+    },
+     {
+        id: "cskill5",
+        name: "STOP", 
+        description: "If your emotions are feeling overwhelming, try this: Stop, and take a pause. It can be really hard, but try tensing your muscles for a second. Take a break and walk away from the situation to slow down and self-regulate. Observe any inner sensations that are happening, and what is happening in your surroundings. Does anything relate to how you feel? Then, proceed mindfully. Think about what it is that you want from the situation. What is it that you are trying to achieve?",
+        category: "emotion regulation",
+        therapy: "DBT",
+    },
+     {
+        id: cskill6,
+        name: "",
+        description: "",
+        category: "",
+    },
+     {
+        id: "cskill7",
+        name: "",
+        description: "",
+        category: "",
+    },
+     {
+        id: "cskill8",
+        name: "",
+        description: "",
+    },
+     {
+        id: "cskill9",
+        name: "",
+        description: "",
+    },
+     {
+        id: "cskill10",
+        name: "",
+        description: "",
+    },
+     {
+        id: "cskill11",
+        name: "",
+        description: "",
+    },
+     {
+        id: "cskill12",
+        name: "",
+        description: "",
+    }
+]
+
+export default defaultCopingTools;
+
+/* 
+Resources consulted:
+1. https://www.therapistaid.com/therapy-article/grounding-techniques-article
+2. https://www.therapistaid.com/therapy-worksheet/dbt-emotion-regulation-skills
+3. https://www.therapistaid.com/therapy-worksheet/coping-skills-anxiety
+4.https://www.therapistaid.com/therapy-worksheet/becoming-psychologically-flexible
+5. https://dbt.tools/emotional_regulation/stop.php
+
+PHP/IP handouts (physical documents)
+6.
+7.
+8.
+9.
+10.
+11.
+12. 
+
+Categories:
+Mindfulness (3) FULL
+Emotion Regulation (2) 1 space left
+    - Wise Mind
+    - Ask for Help 
+
+Communication (0) 3 spaces left 
+    - "I statements"
+    - "Active listening"
+    - ""
+
+? (0) 3 spaces left
+    - ""
+
+Compiled skills from ACT, DBT, and STAIR 
+
+REVIEW CONTENT AND EDIT MY COPING TOOL DESCRIPTIONS FOR WORDING IF NECESSARY.
+*/
