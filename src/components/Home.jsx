dec41c2b5fe7e5fa29f5212c0f05a95979fd6e32
nsok1007 <nsok1007@gmail.com>
2026-08-19 19:30:35 -0500

    feat: Add Home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,21 @@
+function Home(){
+    return(
+        <div>
+            <header>
+                <h1>Welcome to MoonLit</h1>
+                <h2>A psychoeducational mental health app for adult survivors of childhood trauma</h2> 
+            </header>
+            <main>
+                <p>Overcoming adversity is a common experience we all face in one way or another, but childhood should not be one. Childhood experiences can continue to shape how we understand ourselves and our emotions into adulthood, sometimes without us fully understanding why. This is why MoonLit was made.</p> 
+                <p>Current free resources are commonly tailored towards a specific mental health condition or marketed as a general self-help that is too broad.</p>
+                <p>With MoonLit, you can access our curated set of coping tools or create your own! Our purpose is to address the need for mental health resources that are truly free and derived from evidence-based therapies.</p>
+                <br></br>
+                <br></br>
+            </main>
+        </div>
+    )
+}
+
+export default Home;
+
+//todo: 
