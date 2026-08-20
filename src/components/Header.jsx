0ca59c8ce3099337ab13e061054a191231c7b4fa
nsok1007 <nsok1007@gmail.com>
2026-08-20 15:11:38 -0500

    feat: add route paths to CopingSkills and CopingSkillsDetails components; add nav link to 'Coping Skills'

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,16 +2,20 @@ import {Routes, Route, Link} from 'react-router';
 import Home from './Home';
 import About from './About';
 import Contact from './Contact';
+import CopingSkills from './CopingSkills';
+import CopingSkillsDetails from './CopingSkillDetails';
 
 export default function Header() {
     return(
         <div className="header"> 
           <h1>MoonLit</h1> {/*Figure out how to get h1 and nav elements on SAME line visually*/ }
             <nav>
-                <Link to="./home">Home</Link> <Link to="./about">About</Link> <Link to="./contact">Contact</Link>
+                <Link to="./home">Home</Link> <Link to="./copingskills">Coping Skills</Link> <Link to="./about">About</Link> <Link to="./contact">Contact</Link>
             </nav>
             <Routes>
                 <Route path="/home" element={<Home />} />
+                <Route path="/copingskills" element={<CopingSkills />} />
+                <Route path="/copingskills/:cskillId" element={<CopingSkillsDetails />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/contact" element={<Contact />} />
             </Routes>
