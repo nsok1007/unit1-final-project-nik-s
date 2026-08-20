6e4871741ca550fffaaeece5580270d1fb5c507b
nsok1007 <nsok1007@gmail.com>
2026-08-20 15:08:20 -0500

    feat: add CopingSkills component to display all default skills

---
+++ src/components/CopingSkills.jsx
@@ -0,0 +1,19 @@
+import {Link} from 'react-router';
+import mockData from '../mockData';
+
+export default function CopingSkills(){ 
+    return(
+        <div>
+            <ul>
+                {mockData.map((cskill) => (
+                    <li key={cskill.id}>
+                        <Link to={`/copingskills/${cskill.id}`}>
+                        {cskill.name}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+            
+        </div>
+    )
+}
