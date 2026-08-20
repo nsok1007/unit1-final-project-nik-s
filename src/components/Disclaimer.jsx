642df973655ea77b029c7fc75d67b32520f9cd83
nsok1007 <nsok1007@gmail.com>
2026-08-19 19:31:48 -0500

    feat: Add Disclaimer component

---
+++ src/components/Disclaimer.jsx
@@ -0,0 +1,8 @@
+export default function Disclaimer(){
+    return(
+        <div>
+            <p><em><b>Disclaimer:</b> This app is purely educational and DOES NOT replace help from a mental health professional.</em></p>
+            <br></br>
+        </div>
+    )
+}
