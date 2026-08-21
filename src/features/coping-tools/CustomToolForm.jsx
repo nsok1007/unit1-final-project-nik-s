35d5d760aa967d67739b94037170b90e9064bee6
nsok1007 <nsok1007@gmail.com>
2026-08-21 11:05:59 -0500

    wip: add CustomToolForm component

---
+++ src/features/coping-tools/CustomToolForm.jsx
@@ -0,0 +1,57 @@
+import {useState} from 'react';
+import ReusableButton from '../../components/ReusableButton';
+
+export default function CustomToolForm(){
+    const [customTool, setCustomTool] = useState({
+        title: "",
+        description: "",
+    });
+
+    const handleCustomToolChange = (e) => {
+        const{name, value} = e.target;
+        console.log(`Updating ${name}:`, value);
+        setCustomTool((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
+
+    function createTool(){
+         <button type="submit"></button>
+          console.log(createTool);   //console test for button -- CURRENTLY BROKEN
+    }
+
+    return(
+        <div>
+
+            <form>
+                <h2>Create Your Own Coping Tool</h2>
+                <label>
+                    Name your tool: 
+                    <br></br>
+                    <input
+                        type="text"
+                        name="title"
+                        value={customTool.title}
+                        onChange={handleCustomToolChange}
+                        placeholder="Self Soothe w/ Five Senses"
+                    />
+                </label>
+                <br></br>
+                <label>
+                    Describe how you would use this:
+                    <br></br>
+                    <input
+                        type="text"
+                        name="description"
+                        value={customTool.description}
+                        onChange={handleCustomToolChange}
+                        placeholder="Vision: Look at the flowers outside. Hearing: Listen to calming music. Smell: Put lavander oil in the diffuser. Taste: Have a small piece of candy and savor the flavor and texture. Touch: Hold my favorite plushie in my hands and notice how it feels."
+                    />      
+                </label>
+                <br></br>
+                <ReusableButton onClick={createTool} text={"Create"} />
+            </form>    
+        </div> 
+    );
+};
