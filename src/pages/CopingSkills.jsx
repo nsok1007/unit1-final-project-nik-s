f65affb61a81e07e72ecd29adf04bd1d9d561125
nsok1007 <nsok1007@gmail.com>
2026-08-24 15:26:45 -0500

    feat: wip reorganize code, import components; add useState and prop for child component (ReusableItem)

--- src/pages/CopingSkills.jsx
+++ src/pages/CopingSkills.jsx
@@ -1,19 +1,33 @@
-import {Link} from 'react-router';
+import {useNavigate, Link} from 'react-router';
+import ReusableButton from '../components/ReusableButton';
+import ReusableItem from '../components/ReusableItem';
 import mockData from '../features/coping-tools/mockData.json'
 
+export default function CopingSkills({favoriteTool, handleFavoriteToolOnClick}){ 
+    const toCustomToolForm = useNavigate();
+    const handlePageRedirect = () => {
+    toCustomToolForm('/features/coping-tools/customtoolform');
+    };
 
-export default function CopingSkills(){
     return(
-        <div>
-            <ul>
-                {mockData.map((cskill) => (
+   <div>
+        <h1>Coping Skill</h1>
+        <ul>
+            {mockData.map((cskill) => (
                     <li key={cskill.id}>
                         <Link to={`/copingskills/${cskill.id}`}>
                         {cskill.name}
                         </Link>
+                        <ReusableButton onClick={() => handleFavoriteToolOnClick(cskill)} text={"Add +"} />
                     </li>
                 ))}
-            </ul>
-        </div>
-    )
+                <ReusableButton onClick={handlePageRedirect} text={"New Tool +"} />
+       </ul>
+       <ul>
+                {favoriteTool.map((cskill) => (
+                <ReusableItem key={cskill.id} cskill={cskill} />
+                ))}
+       </ul>
+   </div>
+   )   
 }
