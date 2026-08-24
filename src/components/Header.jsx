195a600e53c9c6115f9cb7ba89de04a13202b8ab
nsok1007 <nsok1007@gmail.com>
2026-08-24 16:54:20 -0500

    feat: update Route and import paths for UserLibrary (formerly UserInventory)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ import Contact from '../pages/Contact';
 import CopingSkills from '../pages/CopingSkills';
 import CopingSkillsDetails from '../pages/CopingSkillDetails';
 import CustomToolForm from '../features/coping-tools/CustomToolForm';
-import UserInventory from '../pages/UserInventory';
+import UserLibrary from '../pages/UserLibrary';
 
 export default function Header() {
     const [favoriteTool, setFavoriteTool] = useState([]);
@@ -16,24 +16,39 @@ export default function Header() {
             ...favoriteTool, cskill
         ]);
     }
+    const HandleOnDelete = (id) => {
+        console.log(id);
+        };
+        
 
     return(
         <div className="header"> 
           <h1>MoonLit</h1> {/*Figure out how to get h1 and nav elements on SAME line visually*/ }
             <nav>
-                <Link to="./home">Home</Link> <Link to="./copingskills">Coping Skills</Link> <Link to="./userinventory">User Library</Link><Link to="./about">About</Link> <Link to="./contact">Contact</Link>
+                <Link to="./home">Home</Link> <Link to="./copingskills">Coping Skills</Link> <Link to="./userlibrary">User Library</Link><Link to="./about">About</Link> <Link to="./contact">Contact</Link>
             </nav>
             <Routes>
                 <Route path="/" element={<Home />} /> {/*Default path which is currently set to home */}
                 <Route path="/home" element={<Home />} />
                 <Route path="/copingskills" element={
-                    <CopingSkills handleFavoriteToolOnClick={handleFavoriteToolOnClick}
-                    favoriteTool={favoriteTool} />}
+                    <CopingSkills 
+                        handleFavoriteToolOnClick={handleFavoriteToolOnClick}
+                        favoriteTool={favoriteTool} />
+                    }
                 />
                 <Route path="/copingskills/:cskillId" element={<CopingSkillsDetails />} />
                 <Route path="/features/coping-tools/customtoolform" element={
-                    <CustomToolForm favoriteTool={favoriteTool} setFavoriteTool={setFavoriteTool} />} />
-                <Route path="/userinventory" element={<UserInventory favoriteTool={favoriteTool}/>} />
+                    <CustomToolForm 
+                        favoriteTool={favoriteTool} 
+                        setFavoriteTool={setFavoriteTool} />
+                    }
+                />
+                <Route path="/userlibrary" element={
+                    <UserLibrary
+                        favoriteTool={favoriteTool}
+                        onDelete={HandleOnDelete}/>
+                    }
+                />
                 <Route path="/about" element={<About />} />
                 <Route path="/contact" element={<Contact />} />
             </Routes>
