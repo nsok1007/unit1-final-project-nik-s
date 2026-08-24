5c8133b55ffba71890d66bf5a6b3923a4153ebbe
nsok1007 <nsok1007@gmail.com>
2026-08-23 20:33:12 -0500

    feat: add back button feature

---
+++ src/pages/CopingSkillDetails.jsx
@@ -0,0 +1,30 @@
+import {useParams, useNavigate} from 'react-router';
+import mockData from '../features/coping-tools/mockData.json';
+import ReusableButton from '../components/ReusableButton'
+import ErrorMessage from '../components/ErrorMessage';
+
+export default function CopingSkillsDetails(){  
+   const{cskillId} = useParams(); 
+   const cskill = mockData.find((cskill) => cskill.id === cskillId); 
+   //Inside mockData, find the object and see if the property is equal to the URL parameter
+
+   const toCopingSkills = useNavigate();
+   const handleNavBack = () => {
+   toCopingSkills('../copingskills');
+   };
+
+   if (!cskill) {
+    return (
+        <ErrorMessage>
+            <p> Task not available.</p>
+        </ErrorMessage>
+    );
+   } else {
+        return(
+            <div>
+                <p className="detail-description">description: {cskill.description}</p>
+                <ReusableButton onClick={handleNavBack} text={"Back"} />
+            </div>
+   )};
+};
+
