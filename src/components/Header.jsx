38473b123a2d6264363cace3ae7c9a2d5f4baf0c
nsok1007 <nsok1007@gmail.com>
2026-08-23 20:36:07 -0500

    feat: update Route paths for UserInventory component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,15 +3,16 @@ import Home from '../pages/Home';
 import About from '../pages/About';
 import Contact from '../pages/Contact';
 import CopingSkills from '../pages/CopingSkills';
-import CopingSkillsDetails from '../features/coping-tools/CopingSkillDetails';
+import CopingSkillsDetails from '../pages/CopingSkillDetails';
 import CustomToolForm from '../features/coping-tools/CustomToolForm';
+import UserInventory from '../pages/UserInventory';
 
 export default function Header() {
     return(
         <div className="header"> 
           <h1>MoonLit</h1> {/*Figure out how to get h1 and nav elements on SAME line visually*/ }
             <nav>
-                <Link to="./home">Home</Link> <Link to="./copingskills">Coping Skills</Link> <Link to="./about">About</Link> <Link to="./contact">Contact</Link>
+                <Link to="./home">Home</Link> <Link to="./copingskills">Coping Skills</Link> <Link to="./userinventory">My Library</Link> <Link to="./about">About</Link> <Link to="./contact">Contact</Link>
             </nav>
             <Routes>
                 <Route path="/" element={<Home />} /> {/*Default path which is currently set to home */}
@@ -19,6 +20,7 @@ export default function Header() {
                 <Route path="/copingskills" element={<CopingSkills />} />
                 <Route path="/copingskills/:cskillId" element={<CopingSkillsDetails />} />
                 <Route path="/features/coping-tools/customtoolform" element={<CustomToolForm />} />
+                <Route path= "/UserInventory" element={<UserInventory />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/contact" element={<Contact />} />
                 
