95e021cf63ab993fddd9def059e0722624f5bb81
nsok1007 <nsok1007@gmail.com>
2026-08-24 15:27:26 -0500

    feat: wip -- renders ReusableItem

--- src/pages/UserInventory.jsx
+++ src/pages/UserInventory.jsx
@@ -1,45 +1,22 @@
-import {useState} from 'react';
-import {useNavigate, Link} from 'react-router';
-import ReusableButton from '../components/ReusableButton';
-import mockData from '../features/coping-tools/mockData.json'
+// import CopingSkills from "./CopingSkills";
 
-export default function UserInventory(){ 
-    const [favoriteTool, setFavoriteTool] = useState([]);
-    
-    const handleFavoriteToolOnClick = (cskill) => {
-        setFavoriteTool([
-            ...favoriteTool, cskill
-        ]);
-    }
+import ReusableItem from "../components/ReusableItem";
 
-    const toCustomToolForm = useNavigate();
-    const handlePageRedirect = () => {
-    toCustomToolForm('/features/coping-tools/customtoolform');
-    };
-
+export default function UserInventory({favoriteTool}){
     return(
-   <div>
-        <ul>
-            {mockData.map((cskill) => (
-                    <li key={cskill.id}>
-                        <Link to={`/copingskills/${cskill.id}`}>
-                        {cskill.name}
-                        </Link>
-                        <ReusableButton onClick={() => handleFavoriteToolOnClick(cskill)} text={"Add +"} />
-                    </li>
-                ))}
-                <ReusableButton onClick={handlePageRedirect} text={"New Tool +"} />
-       </ul>
-        <ul>
+        <div>
             {favoriteTool.map((cskill) => (
-                    <li key={cskill.id}>
-                        <Link to={`/copingskills/${cskill.id}`}>
-                        {cskill.name}
-                        </Link>
-                    </li>
-                ))}
-        </ul>
-   </div>
-   )   
+                <ReusableItem key={cskill.id} cskill={cskill} />
+            ))}
+        </div>
+    )
 }
 
+/*
+STATUS: FORM CURRENTLY ACCEPTS AND STORES DATA IN STATE; COPING SKILLS ADDED ALSO STORED IN STATE
+
+TODO: CustomToolForm allows users to edit their custom coping skill (edit button)
+TODO: UserInventory allows users to delete DEFAULT STATIC coping skills (delete button)
+TODO: UserInventory allows users to delete CUSTOM COPING SKILLS (delete button #2)
+TODO: Simulate storing data in an API using localStorage hook to achieve above
+*/
