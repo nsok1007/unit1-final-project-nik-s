4cd2e7196656bec18cbfc5ee42ba4ae554745994
nsok1007 <nsok1007@gmail.com>
2026-08-23 20:43:45 -0500

    chore: update disclaimer

--- src/components/Disclaimer.jsx
+++ src/components/Disclaimer.jsx
@@ -1,7 +1,7 @@
 export default function Disclaimer(){
     return(
         <div>
-            <p><em><b>Disclaimer:</b> This app is purely educational and DOES NOT replace help from a mental health professional.</em></p>
+            <p><em><b>Disclaimer:</b> This app is purely educational and DOES NOT replace professional mental health care from licensed practitioners.</em></p>
             <br></br>
         </div>
     )
