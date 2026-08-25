d1a37c9767d8c72b49e57d8bdc002ce33ceabd3e
nsok1007 <nsok1007@gmail.com>
2026-08-24 20:23:55 -0500

    feat: pass prop from parent to add custom id for user-created skills to allow rendering of descriptions

--- src/pages/CopingSkillDetails.jsx
+++ src/pages/CopingSkillDetails.jsx
@@ -3,9 +3,12 @@ import mockData from '../features/coping-tools/mockData.json';
 import ReusableButton from '../components/ReusableButton'
 import ErrorMessage from '../components/ErrorMessage';
 
-export default function CopingSkillsDetails(){  
+export default function CopingSkillsDetails({favoriteTool}){  
    const{cskillId} = useParams(); 
-   const cskill = mockData.find((cskill) => cskill.id === cskillId); 
+   const userFavoritesAll = mockData.concat(favoriteTool)
+   console.log(userFavoritesAll);
+   const cskill = userFavoritesAll.find((cskill) => cskill.id === cskillId); 
+    // const cskill = mockData.find((cskill) => cskill.id === cskillId); 
    //Inside mockData, find the object and see if the property is equal to the URL parameter
 
    const toCopingSkills = useNavigate();
@@ -22,9 +25,9 @@ export default function CopingSkillsDetails(){
    } else {
         return(
             <div>
-                <h1>{cskill.name}</h1>
-                <p className="detail-description"><strong>Description:</strong> {cskill.description}</p>
-
+                <h1>Coping Skill Details</h1>
+                <p className="detail-name"><strong>Name:</strong> {cskill.name}</p>
+                <p className="detail-description"><strong>Description:</strong> {cskill.description}</p> <br></br>
                 <ReusableButton onClick={handleNavBack} text={"Back"} />
             </div>
    )};
