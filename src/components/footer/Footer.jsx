fe381415fbba26924509ba5087642109d2603df9
nsok1007 <nsok1007@gmail.com>
2026-08-26 21:05:35 -0500

    feat: style Footer component and update paths

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import './footer.css'
+export default function Footer(){
+    return(
+        <div>
+            <footer>
+                <address>
+                    <a href="mailto:sokoln12@gmail.com" target="_blank">Contact</a> &bull; <a href="https://www.github.com/nsok1007" target="_blank">GitHub</a>
+                </address>
+                <p className="copyright"> © 2026 Designed by <a href="https://nsokdesigns.com" target="_blank" rel="noopener noreferrer"> Nik Sokol </a> | All rights reserved.</p>
+            </footer>   
+        </div>
+    );
+};
