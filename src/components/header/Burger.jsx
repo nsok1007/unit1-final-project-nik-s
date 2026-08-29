8d897d9386c7520a51a62d69db8d3bcf045f708e
nsok1007 <nsok1007@gmail.com>
2026-08-28 22:40:04 -0500

    feat: style add hamburger menu and media queries

---
+++ src/components/header/Burger.jsx
@@ -0,0 +1,30 @@
+import {useState} from 'react';
+import {Link} from 'react-router';
+import ReusableButton from '../../components/ReusableButton'
+import './burger.css'
+
+export default function BurgerMenu(){
+
+    const [hamburgerMenu, setHamburgerMenu] = useState(false);
+
+    const handleClick = () => {
+    setHamburgerMenu(!hamburgerMenu);
+    };
+
+    return(
+        <div className="menu-container">
+            <div className="menu-button">
+                <ReusableButton onClick={handleClick} text={"☰"} style={{fontSize: "xx-large"}} />
+            </div>
+            <ul className="menu-links" style={{display: hamburgerMenu ? 'block' : 'none'}} >
+                <li><Link to="./copingskills">Coping Skills</Link></li>
+                <li><Link to="./userlibrary">User Library</Link></li>
+                <li><Link to="./about">About</Link></li>
+                <li><Link to="./contact">Contact</Link></li>
+            </ul>
+        </div>
+    )
+}
+
+
+
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,16 +1,22 @@
 import {NavLink} from 'react-router';
+import Burger from './Burger';
 import '../header/header.css'
 
 export default function Header() { 
     return(
         <div className="header">
             <nav className="nav-bar">
+                <div className="nav-default">
                     <NavLink to="./home"><h1 className="site-name">MoonLit</h1></NavLink>
+                </div>
+                <div className="nav-burger">
                     <NavLink to="./copingskills">Coping Skills</NavLink>
                     <NavLink to="./about">About</NavLink>
                     <NavLink to="./contact">Contact</NavLink>
-                <div className="nav-right">
                     <NavLink to="./userlibrary">User Library</NavLink>
+                </div>
+                <div className="burger-section">
+                    <Burger/>
                 </div>
             </nav>
         </div>
