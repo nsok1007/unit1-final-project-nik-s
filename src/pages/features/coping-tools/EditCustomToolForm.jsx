e5f33bca9a773cc2e11dfd202d1c13f718f748f8
nsok1007 <nsok1007@gmail.com>
2026-09-01 14:20:10 -0500

    chore: add comments

--- src/pages/features/coping-tools/EditCustomToolForm.jsx
+++ src/pages/features/coping-tools/EditCustomToolForm.jsx
@@ -5,14 +5,18 @@ import ReusableButton from '../../../components/ReusableButton';
 
 export default function EditCustomToolForm({favoriteTool, setFavoriteTool, handleNavBack}){
     const{cskillId} = useParams(); 
-    const toolToEdit = favoriteTool.find((cskill) => ( //.find() iterates through favoriteTool for the first match of a skill
+    //.find() iterates through favoriteTool for the first match of a skill
+    const toolToEdit = favoriteTool.find((cskill) => (
         cskill.id === cskillId
     ))
 
     const [editedSkill, setEditedSkill] = useState(toolToEdit) //set useState for editing skill
-
+        
+    // newList and favoriteTool point to same objects in list as references
     const newList = [...favoriteTool] 
-    const filteredList = newList.filter((cskill) => cskill.id != cskillId) //.filter() iterates through newList - a shallow copy of favoriteTool based on skills that match ids
+    
+    //.filter() iterates through newList - a shallow copy of favoriteTool based on skills that match ids
+    const filteredList = newList.filter((cskill) => cskill.id != cskillId) 
      
     const handleEditForm = (e) => { 
         const{name, value} = e.target;
@@ -26,9 +30,12 @@ export default function EditCustomToolForm({favoriteTool, setFavoriteTool, handl
 
     const editTool = (e) => {
         e.preventDefault();
-        setFavoriteTool(() => [
-            ...filteredList, editedSkill
-        ]);
+        //if/else statement to verify that the editedSkill input lengths are not empty before saving/submitting
+        if (editedSkill.name.length > 1 && editedSkill.description.length > 1) {
+            setFavoriteTool(() => [
+                ...filteredList, editedSkill
+            ]);
+        } else console.error(`Invalid coping skill. Please provide a name and description.`);
     }
    
     return(
@@ -41,8 +48,8 @@ export default function EditCustomToolForm({favoriteTool, setFavoriteTool, handl
                     <input
                         type="text"
                         name="name"
-                        onChange={handleEditForm}
                         value={editedSkill.name} 
+                        onChange={handleEditForm}
                     />
                 </label>
                 <br></br>
@@ -51,8 +58,8 @@ export default function EditCustomToolForm({favoriteTool, setFavoriteTool, handl
                     <br></br>
                     <textarea
                         name="description"
-                        onChange={handleEditForm}
                         value={editedSkill.description}
+                        onChange={handleEditForm}
                     />
                     <div className="button">
                         <ReusableButton onClick={editTool} text={"Save"} style={{marginTop: "10px", width: "100px", height: "25px", textAlign: "center", alignContent: "left", justifyContent: "left", borderRadius: "8px", border: "none"}} /> 
