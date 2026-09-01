6ca58978450115ba49a2be0cd61216e2501ca599
nsok1007 <nsok1007@gmail.com>
2026-09-01 14:11:17 -0500

    chore: delete comment

--- src/pages/UserLibrary.jsx
+++ src/pages/UserLibrary.jsx
@@ -24,7 +24,3 @@ export default function UserLibrary({favoriteTool, handleOnDelete, handleNavEdit
         
     )
 }
-
-/*
-NOTE: user favorited tools are stored in memory --> use persistent state/local storage to keep on UserInventory --> DELETE a favorite tool from memory w/out re-rendering UserInventory each time and having users re-add their skills upon every refresh
-*/
